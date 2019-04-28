2a17a828766426b53debd68bfc049729a7bf8ed7
Amir RahimZade <amir.rhm93@gmail.com>
2019-04-28 16:39:38 +0430

    add bootstrap

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -8,6 +8,13 @@ import VueI18n from 'vue-i18n';
 import VeeValidate from 'vee-validate';
 import messagesFa from './lang/validation/fa';
 import './router';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'node_modules/bootstrap/scss/bootstrap';
+import 'node_modules/bootstrap-vue/src/index.scss';
+import 'custom.scss';
+
 
 require('es6-promise').polyfill();
 window._ = lodash;
